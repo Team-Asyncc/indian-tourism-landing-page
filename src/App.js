300a1c60a58daa6082ecbbf999c9e8dc9b05eef1
VivekBcloud <20vivektoppo@gmail.com>
2021-07-18 10:20:59 +0530

    layout done 🎐

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.scss'
-import Navbar from './Components/Navbar/Navbar'
-import PopularPlaces from './Components/PopularPlaces/PopularPlaces'
-import Footer from './Components/Footer/Footer'
-
+import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar";
+import PopularPlaces from "./Components/PopularPlaces/PopularPlaces";
+import Footer from "./Components/Footer/Footer";
+import Experiance from "./Components/Experiance/Experiance";
 function App() {
   return (
     <div>
       <Navbar />
       <PopularPlaces />
       <Footer />
+      <Experiance />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Experiance/Experiance.jsx
+++ src/Components/Experiance/Experiance.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./experiance.scss";
 const Experiance = () => {
-  return <div></div>;
+  const ExpCard = ({ exp }) => {
+    console.log(`url${exp.url}`);
+    return (
+      <div className="exp-card" style={{ backgroundImage: `url(${exp.url})` }}>
+        <p className="exp-name">{exp.name}</p>
+      </div>
+    );
+  };
+  const demoData = [
+    {
+      name: "Heritage",
+      url: "https://www.incredibleindia.org/content/dam/incredible-india-v2/images/experiences/spiritual/buddhism/1920-1080.jpg/jcr:content/renditions/cq5dam.web.512.384.jpeg",
+    },
+    {
+      name: "Nature and Wildlife",
+      url: "https://www.incredibleindia.org/content/dam/incredible-india-v2/images/experiences/nature/1920-1080.jpg/jcr:content/renditions/cq5dam.web.512.384.jpeg",
+    },
+  ];
+  return (
+    <div className="experiance">
+      <h1>Immersive Experiance</h1>
+      <p>In the land of opulence, let loose and discover yourself</p>
+      <div className="cards">
+        {demoData.map((exp) => (
+          <ExpCard exp={exp} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Experiance;
