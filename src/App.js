906364df5b5a1cfa47cad1931c82bcfb9f975194
ashirbad <ashirbadbehera29@gmail.com>
2021-07-18 12:34:16 +0530

    ⚡️ add prettierrc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.scss";
-import Navbar from "./Components/Navbar/Navbar";
-import PopularPlaces from "./Components/PopularPlaces/PopularPlaces";
-import Footer from "./Components/Footer/Footer";
+import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
+import PopularPlaces from './Components/PopularPlaces/PopularPlaces';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <PopularPlaces />
-      <Footer />
+      {/* <PopularPlaces />
+      <Footer /> */}
     </div>
   );
 }
