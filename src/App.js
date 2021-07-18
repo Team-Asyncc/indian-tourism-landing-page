8faa5d9131510978e8614f4f01620c8065107f0a
ashirbad <ashirbadbehera29@gmail.com>
2021-07-18 17:41:22 +0530

    🚀 add big caraousel

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.scss';
 import Navbar from './Components/Navbar/Navbar';
 import PopularPlaces from './Components/PopularPlaces/PopularPlaces';
 import Footer from './Components/Footer/Footer';
+import BigCarousel from './Components/Carousel/BigCarousel';
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <PopularPlaces />
-      <Footer /> */}
+      <BigCarousel />
     </div>
   );
 }
