dc4597c4abf4fbf033f5af0530b1d9f2a52ce880
shrey maurya <shreymaurya2000@gmail.com>
2021-07-18 21:07:50 +0530

    popular-trips-section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss'
 import Navbar from './Components/Navbar/Navbar'
 import PopularPlaces from './Components/PopularPlaces/PopularPlaces'
 import Footer from './Components/Footer/Footer'
+import { PopularTrips } from './Components/PopularTrips/PopularTrips'
 
 function App() {
   return (
     <div>
       <Navbar />
       <PopularPlaces />
+      <PopularTrips/>
       <Footer />
     </div>
   )
---
+++ src/Components/PopularPlaces/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (title,) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Card;
