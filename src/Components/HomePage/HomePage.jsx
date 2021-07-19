b80250290226d38446cea9ab1dd377af11153f60
VivekBcloud <20vivektoppo@gmail.com>
2021-07-19 14:04:46 +0530

    added video in homepage 📺

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './homepage.scss';
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="home-page">
+      <video src="./assets/videos/nature-video.mp4" loop autoPlay></video>
+      <div class="overlay"></div>
+      <div class="text">
+        <h2>Never Stop To </h2>
+        <h3>Exploring The World</h3>
+        <a href="#">Explore</a>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
