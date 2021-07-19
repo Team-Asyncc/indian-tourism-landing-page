1fa263c428f2b4b17f72733c9bd46e47ad99f5b9
Abhinav <aaricrubel@gmail.com>
2021-07-19 16:58:24 +0530

    Fixed Homepage CSS

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -6,9 +6,11 @@ const HomePage = () => {
       <video src="./assets/videos/nature-video.mp4" muted loop autoPlay></video>
       <div className="overlay"></div>
       <div className="text">
-        <h2>Never Stop To </h2>
-        <h3>Exploring The World</h3>
-        <a href="https://www.incredibleindia.org/content/incredible-india-v2/en/destinations/popular-destinations.html">Explore</a>
+        <h3>Never Stop From </h3>
+        <h4>Exploring The World</h4>
+        <a href="https://www.incredibleindia.org/content/incredible-india-v2/en/destinations/popular-destinations.html">
+          Explore
+        </a>
       </div>
     </div>
   );
